6abc3dfe05f494af0e870b9ea768333336d99751
AsP3X <zombymedia@gmaill.com>
2020-11-19 17:23:26 +0100

    TASK: add delete BankAccount and Restore BankAccount via backupcode

--- services/api/user/modules/bank/deleteAccount.php
+++ services/api/user/modules/bank/deleteAccount.php
@@ -1,3 +1,11 @@
+<!--
+####################################################################
+## ZOUPA - (ZombyMediaIC open source usage protection agreement)  ##
+## License as of: 10.05.2020 19:41 | #202005101941                ##
+## Niklas Vorberg (AsP3X)                                         ##
+####################################################################
+-->
+
 <?php
 
   function getCredits($conn, $bid) {
